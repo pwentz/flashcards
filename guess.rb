8e4e53adad935f202af5c23435c18d724838c064
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-17 11:51:50 -0600

    testing passed, ready for runner

---
+++ guess.rb
@@ -0,0 +1,32 @@
+class Guess
+  attr_reader     :response,
+                  :card
+
+
+  def initialize(response, card)
+    @response = response
+    @card = card
+  end
+
+  def card
+    @card
+  end
+
+  def correct?
+    if @response == card.answer
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if @response == card.answer
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+
+
+end
