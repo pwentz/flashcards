2a56bbc2d77f471b92655b6de9f0aa341ad63f95
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-17 17:47:43 -0600

    changed record guess function to output feedback

--- round.rb
+++ round.rb
@@ -13,11 +13,13 @@ class Round
 
   def record_guess(guess)
     new_guess = Guess.new(guess, @current_card)
+    # require "pry"; binding.pry
     @guesses.push(new_guess)
     if new_guess.response == @current_card.answer
       @number_correct += 1
     end
-    new_guess.feedback
+    puts new_guess.feedback
+    puts new_guess.card.answer
     @deck.cards.shift
     @current_card = @deck.cards.first
   end
