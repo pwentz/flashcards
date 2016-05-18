d309901463dc5e19b8042096a79cdde2fbe19d7c
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-17 18:15:04 -0600

    changed load_deck method to cards

--- card_generator.rb
+++ card_generator.rb
@@ -12,7 +12,7 @@ class CardGenerator
     # @cards = load_deck
   end
 
-  def load_deck
+  def cards
     #change method name to cards?
     #if cardGen must assume deck attr,
     #new_deck = Deck.new(CardGenerator.new(filename)) will give you that
