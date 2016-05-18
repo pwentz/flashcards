15f546d650bb4cdac0e2b81b2ad2280f52b80c2f
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-17 18:15:19 -0600

    changed load_deck method to cards

--- card_generator_test.rb
+++ card_generator_test.rb
@@ -12,26 +12,26 @@ class CardGeneratorTest < Minitest::Test
 
   def test_card_generator_loads_cards
     card_generator = CardGenerator.new("trivia_questions.txt")
-    result = card_generator.load_deck
+    result = card_generator.cards
     assert_equal Array, result.class
     assert_equal Card, result.first.class
   end
 
   def test_card_generator_loads_answer
     card_generator = CardGenerator.new("trivia_questions.txt")
-    all_cards = card_generator.load_deck
+    all_cards = card_generator.cards
   assert_equal "Graham's Number", all_cards.first.answer
   end
 
   def test_card_generator_questions_match_answer
     card_generator = CardGenerator.new("trivia_questions.txt")
-    all_cards = card_generator.load_deck
+    all_cards = card_generator.cards
     assert_equal "Pnigerophobia is the fear of what?" , all_cards[1].question
   end
 
   def test_can_deck_receive_cards_from_card_generator
     card_generator = CardGenerator.new("trivia_questions.txt")
-    new_deck = Deck.new(card_generator.load_deck)
+    new_deck = Deck.new(card_generator.cards)
 
     assert_equal Array, new_deck.cards.class
     assert_equal 4, new_deck.cards.count
