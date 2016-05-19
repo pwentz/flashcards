d3a537437e27239ff500fd678fe1ad303ef0743a
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-18 22:05:17 -0600

    removed comments

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -5,19 +5,7 @@ require_relative './round'
 require_relative './card_generator/'
 # require_relative '../trivia_questions.txt'
 require 'pry'
-# card_1 = Card.new("Who hosted Hollywood Squares between 1986 and 1989?", "John Davidson")
-# card_2 = Card.new("A group of lions is called a what?", "Pride")
-# card_3 = Card.new("When was the Apple II computer introduced?", "1977")
-# card_4 = Card.new("What author created the character of Tom Bombadil?", "J.R.R. Tolkien")
-# card_5 = Card.new("Which bird lays the largest eggs?", "Ostrich")
-# card_6 = Card.new("What archaeological site is known as the Lost City of the Incas?", "Machu Picchu")
-# card_7 = Card.new("What is the only country which borders the Black Sea and the Caspian Sea?", "Russia")
-# card_8 = Card.new("What is most likely to occur when your diaphragm goes into spasms?", "Hiccups")
-# card_9 = Card.new("Who did Abraham Lincoln replace as president?", "James Buchanan")
-# card_10 = Card.new("Gangster Al Capone was eventually arrested and convicted on what charge?", "Tax evasion")
 
-#TO DO: Incorporate Card Generator here:
-# new_deck = Deck.new([card_2, card_3, card_6, card_8, card_10])
 new_deck = Deck.new(CardGenerator.new("trivia_questions.txt").cards)
 new_round = Round.new(new_deck)
 
@@ -35,7 +23,6 @@ def start(round)
   puts "****** Game over! ******"
   round.write_file
   puts "Your progress has been saved! Please check your working directory!"
-  # binding.pry
   puts "You had #{round.number_correct} correct guesses out of #{round.guesses.count} for a score of #{round.percent_correct}%!"
 end
 
