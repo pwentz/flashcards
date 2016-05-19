d292f6a8037ffd5b56d65691479f78be83c11c3a
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-18 21:56:36 -0600

    added incorrect loop and write results extenstion

---
+++ lib/guess.rb
@@ -0,0 +1,29 @@
+class Guess
+  attr_reader     :response,
+                  :card
+
+
+  def initialize(response, card)
+    @response = response
+    @card = card
+  end
+
+
+  def correct?
+    if @response == card.answer
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if @response == card.answer
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+
+
+end
