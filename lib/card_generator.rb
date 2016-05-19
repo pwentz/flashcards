e6981fa08c4e5fbe5f0977ffa642056c2515550c
riskybiscuit <patrick.w.wentz@gmail.com>
2016-05-18 22:06:53 -0600

    removed comments

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -11,14 +11,9 @@ class CardGenerator
   end
 
   def cards
-    #change method name to cards?
-    #if cardGen must assume deck attr,
-    #new_deck = Deck.new(CardGenerator.new(filename)) will give you that
     imported_deck = File.readlines(@file).map do |line|
-      # binding.pry
       line.chomp.split(",")
     end
-    # binding.pry
     imported_deck.map do |line|
       Card.new(line[0],line[1])
     end
